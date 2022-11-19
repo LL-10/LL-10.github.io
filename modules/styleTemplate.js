6c4cb8b2f5abfc4bc9f7edd9eb66996d3a07bda6
LL-10 <97639913+LL-10@users.noreply.github.com>
2022-11-19 18:52:16 +0100

    update styleTemplate.js

--- modules/styleTemplate.js
+++ modules/styleTemplate.js
@@ -2,7 +2,7 @@ class StyleTemplate {
   #main
   constructor(input) {
     if (typeof input=='string') {
-      this.#main = Object.fromEntries(input.split(/,|;/).map(x=>x.trim().split(':').map(x=>x.trim()));
+      this.#main = Object.fromEntries(input.split(/,$|;$/)[0].split(/,|;/).map(x=>x.trim().split(':').map(x=>x.trim()));
     } else if (typeof input=='object') {
       this.#main = input;
     }
