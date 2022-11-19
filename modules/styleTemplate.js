c326f7dd788d9554a0618ade301b81448876e29b
LL-10 <97639913+LL-10@users.noreply.github.com>
2022-11-19 18:08:24 +0100

    update styleTemplate.js

--- modules/styleTemplate.js
+++ modules/styleTemplate.js
@@ -2,8 +2,7 @@ class StyleTemplate {
   #main
   constructor(input) {
     if (typeof input=='string') {
-      this.#main = {};
-      
+      this.#main = Object.fromEntries(input.split(',').split(':'));
     } else if (typeof input=='object') {
       this.#main = input;
     }
