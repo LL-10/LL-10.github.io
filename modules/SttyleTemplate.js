6dbb54e6aafe715bb345da0807362bcaadc6127e
LL-10 <97639913+LL-10@users.noreply.github.com>
2022-11-19 19:06:44 +0100

    rename styleTemplate.js

---
+++ modules/SttyleTemplate.js
@@ -0,0 +1,12 @@
+class StyleTemplate {
+  #main
+  constructor(input) {
+    if (typeof input=='string') {
+      this.#main = Object.fromEntries(input.split(/,$|;$/)[0].split(/,|;/).map(x=>x.trim().split(':').map(x=>x.trim()));
+    } else if (typeof input=='object') {
+      this.#main = input;
+    }
+  }
+}
+
+export default StyleTemplate;
