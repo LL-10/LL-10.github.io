a5ec049651890634ca654a578f644afb6d6e27d3
LL-10 <97639913+LL-10@users.noreply.github.com>
2022-11-19 16:24:28 +0100

    update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const HOME = new Page("LL-10", true, ()=>{
  link2.setAttribute('rel', 'stylesheet');
  link2.setAttribute('href', './style/menu.css');
  var menu = body.append('div');
- menu.id = 'menu';
+ menu.setAttribute('id' 'menu');
  var list = menu.append('ul');
  var l1 = list.append('li');
  var l2 = list.append('li');
