c0c99b42ea4c1fb665048b60be52f65d82938a75
LL-10 <lorenzolomartire13@gmail.com>
2023-02-13 12:18:40 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 void async function main() {
 	try {
 		const HTMLElement = await (await import './DevLib/HTML:JS/HTMLElement.js');
-		const body = new HTMLElement(document.body);
+		const html = new HTMLElement(document.documentElement);
 		const head = new HTMLElement(document.head);
 		const icon = head.append('link', null, {
 			rel: 'icon',
@@ -13,6 +13,7 @@ void async function main() {
 			type: 'text/css',
 			href: './style/menu.css'
 		});
+		const body = html.append('body');
 		const menu = body.append('div', null, {
 			id: 'menu',
 			'style*': null
