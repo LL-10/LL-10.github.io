64a6d661c95844b39964cc1b3cf404c8cbd3f52f
LL-10 <lorenzolomartire13@gmail.com>
2023-02-13 11:55:25 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,66 +1,34 @@
-import './modules/default.js';
-import {html, head, title, body} from './modules/HTMLelement.js';
-import Page from './modules/HTMLpage.js';
-import TStyle from './modules/StyleTemplate.js';
-
-const HOME = new Page("LL-10", true, ()=>{
- var link1 = head.append('link');
- var link2 = head.append('link');
- var icon = head.append('link');
- link1.setAttribute('rel', 'stylesheet');
- link1.setAttribute('href', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
- link2.setAttribute('rel', 'stylesheet');
- link2.setAttribute('href', './style/menu.css');
- icon.setAttribute('rel', 'icon');
- icon.setAttribute('type', 'image/png');
- icon.setAttribute('href', 'Resources/main/icon.png');
- var menu = body.append('div');
- menu.tStyle(new TStyle('width: 96%; float: left; margin: 2%; padding: 0; border-top: 3px solid #AABAAF; border-right: 3px solid #AABAAF; border-bottom: 3px solid #AABAAF; background: #DDEADF; font-size: 112.5%;'));
- menu.setAttribute('id', 'menu');
- var list = menu.append('ul');
- var l1 = list.append('li');
- var l2 = list.append('li');
- l1.write('hello');
- var lb = l1.append('ul');
- var ll1 = lb.append('li')
- var othello = ll1.append('a');
- othello.setAttribute('href', './othello');
- othello.init('Othello');
-});
-HOME.build();
-
-
-/*
- var div = body.append('div');
- var username = div.append('input');
- var password = div.append('input');
- var submit = div.append('input');
- var server = body.append('div');
- var host = server.append('input');
- var port = server.append('input');
- 
- username.setAttribute('type','text');
- username.setAttribute('placeholder','Username');
- password.setAttribute('type','password');
- password.setAttribute('placeholder','Password');
- submit.setAttribute('type','submit');
- host.setAttribute('type','text');
- host.setAttribute('placeholder','host [0.0.0.0]');
- port.setAttribute('type','number');
- port.setAttribute('min','1024');
- port.setAttribute('max','65535');
- port.setAttribute('placeholder','port [1024]');
- 
- div.setStyle('margin','3px 0');
- server.setStyle('margin','3px 0');
- username.setStyle('width','132px');
- username.setStyle('margin','0 1px');
- password.setStyle('width','132px');
- password.setStyle('margin','0 1px');
- submit.setStyle('width','66px');
- submit.setStyle('margin','0 1px');
- host.setStyle('width','132px');
- host.setStyle('margin','0 1px');
- port.setStyle('width','132px');
- port.setStyle('margin','0 1px');
- */
+void async function main() {
+	try {
+		const HTMLElement = await (await import './DevLib/HTML:JS/HTMLElement.js');
+		const body = new HTMLElement(document.body);
+		const head = new HTMLElement(document.head);
+		const icon = head.append('link', null, {
+			rel: 'icon',
+			type: 'image/png',
+			href: './Resources/main/icon.png'
+		});
+		const style = head.append('link', null, {
+			rel: 'stylesheet',
+			type: 'text/css',
+			href: './style/menu.css'
+		});
+		var menu = body.append('div', null, {
+			id: 'menu',
+			'style*': null
+		});
+		menu.style = 'width: 96%; float: left; margin: 2%; padding: 0; border-top: 3px solid #AABAAF; border-right: 3px solid #AABAAF; border-bottom: 3px solid #AABAAF; background: #DDEADF; font-size: 112.5%;';
+		var list = menu.append('ul');
+		var l1 = list.append('li');
+		var l2 = list.append('li');
+		l1.write('hello');
+		var lb = l1.append('ul');
+		var ll1 = lb.append('li')
+		var othello = ll1.append('a', null, {
+			href: './othello',
+		});
+		othello.write('Othello');
+	} catch (e) {
+		alert (e)
+	}
+}
