d5b6fa29a6dc88e2d48a134cbad0035fe725dc74
LL-10 <lorenzolomartire13@gmail.com>
2023-02-13 12:50:24 +0100

    modified:   script.js 	new file:   script2.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-void async function main() {
+void async function () {
 	try {
 		const HTMLElement = await (await import('./DevLib/HTML:JS/HTMLElement.js')).default();
 		const html = new HTMLElement(document.documentElement);
@@ -13,21 +13,8 @@ void async function main() {
 			type: 'text/css',
 			href: './style/menu.css'
 		});
-		const body = html.append('body');
-		const menu = body.append('div', null, {
-			id: 'menu',
-			'style*': null
-		});
-		menu.style = 'width: 96%; float: left; margin: 2%; padding: 0; border-top: 3px solid #AABAAF; border-right: 3px solid #AABAAF; border-bottom: 3px solid #AABAAF; background: #DDEADF; font-size: 112.5%;';
-		const list = menu.append('ul');
-		const l1 = list.append('li').write('hello');
-		const l2 = list.append('li');
-		const lb = l1.append('ul');
-		const ll1 = lb.append('li')
-		const othello = ll1.append('a', null, {
-			href: './othello',
-		}).write('Othello');
+		const body = html.append('body').write('hello');
 	} catch (e) {
-		alert (e)
+		alert (e);
 	}
-}
+};
