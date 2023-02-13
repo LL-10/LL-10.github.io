00e4db4622225127f3b02c227706ee3dac4c970b
LL-10 <lorenzolomartire13@gmail.com>
2023-02-13 11:58:49 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -13,21 +13,19 @@ void async function main() {
 			type: 'text/css',
 			href: './style/menu.css'
 		});
-		var menu = body.append('div', null, {
+		const menu = body.append('div', null, {
 			id: 'menu',
 			'style*': null
 		});
 		menu.style = 'width: 96%; float: left; margin: 2%; padding: 0; border-top: 3px solid #AABAAF; border-right: 3px solid #AABAAF; border-bottom: 3px solid #AABAAF; background: #DDEADF; font-size: 112.5%;';
-		var list = menu.append('ul');
-		var l1 = list.append('li');
-		var l2 = list.append('li');
-		l1.write('hello');
-		var lb = l1.append('ul');
-		var ll1 = lb.append('li')
-		var othello = ll1.append('a', null, {
+		const list = menu.append('ul');
+		const l1 = list.append('li').write(hello);
+		const l2 = list.append('li');
+		const lb = l1.append('ul');
+		const ll1 = lb.append('li')
+		const othello = ll1.append('a', null, {
 			href: './othello',
-		});
-		othello.write('Othello');
+		}).write('Othello');
 	} catch (e) {
 		alert (e)
 	}
