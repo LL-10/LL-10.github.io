e5c49b6ca8a8043c06d7e47ae0054736a481a6f2
LL-10 <lorenzolomartire13@gmail.com>
2023-02-23 09:01:17 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const html = new HTMLElement(document.documentElement);
-const head = new HTMLElement(document.head);
-const icon = head.append('link', null, {
+const main = new HTMLDocument((body, head) => {
+ const icon = head.append('link', null, {
 	rel: 'icon',
 	type: 'image/png',
 	href: './Resources/main/icon.png'
 });
-const style = head.append('link', null, {
+ const style = head.append('link', null, {
 	rel: 'stylesheet',
 	type: 'text/css',
 	href: './style/menu.css'
 });
-const body = html.append('body');
-const menu = body.append('div', null, {
+ const menu = body.append('div', null, {
 	id: 'menu',
 	'style*': null
 });
@@ -45,11 +43,13 @@ const menu = body.append('div', null, {
 			}
 		});*/
 //menu.style = 'width: 96%; float: left; margin: 2%; padding: 0; border-top: 3px solid #AABAAF; border-right: 3px solid #AABAAF; border-bottom: 3px solid #AABAAF; background: #DDEADF; font-size: 112.5%;';
-const list = menu.append('ul');
-const l1 = list.append('li').write('hello');
-const l2 = list.append('li');
-const lb = l1.append('ul');
-const ll1 = lb.append('li')
-const othello = ll1.append('a', null, {
-	href: './othello',
-}).write('Othello');
+ const list = menu.append('ul');
+ const l1 = list.append('li').write('hello');
+ const l2 = list.append('li');
+ const lb = l1.append('ul');
+ const ll1 = lb.append('li')
+ const othello = ll1.append('a', null, {
+  href: './othello',
+ }).write('Othello');
+});
+main.load();
