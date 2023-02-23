be7179a529d1844bdd345314c25637c5bcdfb30c
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2023-02-23 17:04:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,28 @@
 const main = new HTMLDocument((body, head) => {
- const icon = head.append('link', null, {
+
+const icon = head.append('link', null, {
 	rel: 'icon',
 	type: 'image/png',
 	href: './Resources/main/icon.png'
 });
- const style = head.append('link', null, {
+const style = head.append('link', null, {
 	rel: 'stylesheet',
 	type: 'text/css',
 	href: './style/menu.css'
 });
+
+ 
  const menu = body.append('div', null, {
-	id: 'menu',
-	'style*': null
-});
-/*Object.defineProperties(menu.style, {
-			width: {
-				value: '96%'
-			},
-			float: {
-				value: 'left'
-			},
-			margin: {
-				value: '2%'
-			},
-			padding: {
-				value: '0'
-			},
-			border-top: {
-				value: '3px solid #AABAAF'
-			},
-			border-right: {
-				value: '3px solid #AABAAF'
-			},
-			border-bottom: {
-				value: '3px solid #AABAAF'
-			},
-			background: {
-				value: '#DDEADF'
-			},
-			font-size: {
-				value: '112.5%'
-			}
-		});*/
-//menu.style = 'width: 96%; float: left; margin: 2%; padding: 0; border-top: 3px solid #AABAAF; border-right: 3px solid #AABAAF; border-bottom: 3px solid #AABAAF; background: #DDEADF; font-size: 112.5%;';
- const list = menu.append('ul');
- const l1 = list.append('li').write('hello');
- const l2 = list.append('li');
- const lb = l1.append('ul');
- const ll1 = lb.append('li')
- const othello = ll1.append('a', null, {
-  href: './othello',
- }).write('Othello');
-});
+  id: 'menu',
+  'style*': null
+ });
+  const list = menu.append('ul');
+  const l1 = list.append('li').write('hello');
+  const l2 = list.append('li');
+  const lb = l1.append('ul');
+  const ll1 = lb.append('li')
+  const othello = ll1.append('a', null, {
+   href: './othello',
+  }).write('Othello');
+}, "LL");
 main.load();
