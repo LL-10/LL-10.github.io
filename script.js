7e6e85a5a81727fb35162439ee4a90e3471941f7
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2023-02-26 21:11:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const menu = new HTMLFragment((menu) => {
+	const div = menu.append('div', null, {
+		id: 'menu',
+		'style*': null
+	});
+	const list = div.append('ul');
+	const l0 = list.append('li').write('HOME').on('click', () => {
+	main.load();
+});
+	const l1 = list.append('li').write('GAMES');
+	const lb = l1.append('ul');
+	const ll1 = lb.append('li')
+	const othelloButton = ll1.append('a', null, {
+		href: 'javascript:void(0)',
+	}).write('Othello').on('click', () => {
+		othello.load();
+	});
+});
+
 const main = new HTMLDocument((body, head) => {
 	const icon = head.append('link', null, {
 		rel: 'icon',
@@ -9,22 +28,10 @@ const main = new HTMLDocument((body, head) => {
 		type: 'text/css',
 		href: './style/menu.css'
 	});
-	const menu = body.append('div', null, {
-		id: 'menu',
-		'style*': null
-	});
-	const list = menu.append('ul');
-	const l1 = list.append('li').write('hello');
-	const l2 = list.append('li');
-	const lb = l1.append('ul');
-	const ll1 = lb.append('li')
-	const othelloButton = ll1.append('a', null, {
-		href: 'javascript:void(0)',
-	}).write('Othello').on('click', () => {
-		othello.load();
-	});
+	menu.use(body);
 }, 'LL');
 const othello = new HTMLDocument((body) => {
+	menu.use(body);
 	body.write('work in progress');
 }, 'LL - Othello');
 main.load();
