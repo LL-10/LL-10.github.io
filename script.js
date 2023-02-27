d2b8601954b5060bbcafd39938f12eb772123b49
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2023-02-27 09:55:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const nav = new HTMLFragment((nav) => {
 		'style*': null
 	});
 	const menu = box.append('ul');
-	const l0 = menu.append('li').write('HOME').on('click', () => {
-	main.load();
+	const HOME = menu.append('li').write('HOME').on('click', () => {
+		home.load();
 });
 	const l1 = menu.append('li').write('GAMES');
 	const lb = l1.append('ul');
 	const ll1 = lb.append('li')
-	const othelloButton = ll1.append('a', null, {
+	const OTHELLO = ll1.append('a', null, {
 		href: 'javascript:void(0)',
 	}).write('Othello').on('click', () => {
 		othello.load();
 	});
 });
 
-const main = new HTMLDocument((body, head) => {
+const home = new HTMLDocument((body, head) => {
 	const icon = head.append('link', null, {
 		rel: 'icon',
 		type: 'image/png',
@@ -35,4 +35,4 @@ const othello = new HTMLDocument((body) => {
 	nav.use();
 	body.write('work in progress');
 }, 'LL - Othello');
-main.load();
+home.load();
