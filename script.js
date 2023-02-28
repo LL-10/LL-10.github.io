13c9a375e01558a4cea438e3b6e836d28e1d4967
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2023-02-28 11:12:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.palette = {
 	background: '#FFFDFB',
 	color: '#333333',
+	grey: '#f3f3f3',
+	red: '#E9A29F',
 	b: '#955555',
 	c: '#F9F2EF',
 };
@@ -8,16 +10,40 @@ document.body.style.background = document.palette.background;
 document.body.style.color = document.palette.color;
 
 const load = function(page) {
-	new HTMLDocument(body => {
-		body.write("Page loading...");
-		
-	}, "LL - loading").load();
+	new HTMLDocument((body, head) => {
+		const icon = head.append('link', null, {
+			rel: 'icon',
+			type: 'image/png',
+			href: './Resources/main/icon.png'
+		});
+		const style = head.append('link', null, {
+			rel: 'stylesheet',
+			type: 'text/css',
+			href: './style/menu.css'
+		});
+		nav.use();
+		const loader = body.append('div', null, ['style*']);
+		const x = 40;
+		loader.style.position = 'absolute';
+		loader.style.top = '50%';
+		loader.style.left = '50%';
+		loader.style.width = x + 'px';
+		loader.style.height = x + 'px';
+		loader.style.margin = '-' + x / 2 + 'px 0 0 -' + x / 2 + 'px';
+		loader.style.borderRadius = '50%';
+		loader.style.border = '14px solid ' + document.palette.grey;
+		loader.style.borderTop = '14px solid ' + document.palette.red;
+		loader.style.transition = 'transform linear 1200ms';
+		setTimeout(()=>{
+			loader.style.transform = 'rotate(1.5turn)';
+		}, 2);
+	}, 'LL - loading').load();
 	setTimeout(()=>{
 		page.load();
 	}, 1200);
 }
 
-const nav = new HTMLFragment((nav) => {
+const nav = new HTMLFragment(nav => {
 	const box = nav.append('div', null, {
 		id: 'menu',
 		'style*': null
@@ -37,21 +63,11 @@ const nav = new HTMLFragment((nav) => {
 	});
 });
 
-const home = new HTMLDocument((body, head) => {
-	const icon = head.append('link', null, {
-		rel: 'icon',
-		type: 'image/png',
-		href: './Resources/main/icon.png'
-	});
-	const style = head.append('link', null, {
-		rel: 'stylesheet',
-		type: 'text/css',
-		href: './style/menu.css'
-	});
+const home = new HTMLDocument(body => {
 	nav.use();
 }, 'LL');
 
-const othello = new HTMLDocument((body) => {
+const othello = new HTMLDocument(body => {
 	nav.use();
 	body.write('work in progress');
 }, 'LL - Othello');
